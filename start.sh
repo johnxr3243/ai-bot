e01956039982abd350f79f0d11f2eb429af8b63b
johnxr3243 <fariismamba@gmail.com>
2026-01-09 07:28:07 +0000

    Final clean build without secrets

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@
 # تشغيل البوت في الخلفية مع تفعيل الـ Unbuffered mode للـ Logs
 python -u bot.py 
 
-# انتظار بسيط للتأكد من بدء البوت
-sleep 2
 
 # تشغيل الموقع (وهو ده اللي Railway هيراقبه)
 python -u main.py
